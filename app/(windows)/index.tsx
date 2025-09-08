86c218975b0195c06418050e0886619702383d2f
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-08 14:35:28 +0530

    add assets

--- app/(windows)/index.tsx
+++ app/(windows)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-black'>
-      <Text className="text-xl font-bold text-white">Welcome</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className="text-xl font-bold ">Welcome</Text>
     </View>
   );
 }
