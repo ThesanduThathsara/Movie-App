6fdde274195a38b3c9bf2eee700e01e3c7d13898
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-10 13:41:39 +0530

    add background image and logo

--- app/(windows)/index.tsx
+++ app/(windows)/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import {Animated, Image, Text, View} from "react-native";
+import {images} from "@/assets/constants/images";
+import ScrollView = Animated.ScrollView;
+import {icons} from "@/assets/constants/icons";
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className="text-xl font-bold ">Welcome</Text>
+    <View className="flex-1 bg-black">
+        <Image source={images.bg} className="absolute w-full h-full z-0"/>  {/* background image*/}
+
+        <ScrollView className="flex-1 px-5">
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>  {/*home page logo*/}
+        </ScrollView>
     </View>
   );
 }
