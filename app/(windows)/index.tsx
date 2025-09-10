7b3ed2aa6fa0f9b24dd16327977b43c5bc40d071
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-10 14:04:08 +0530

    create search component

--- app/(windows)/index.tsx
+++ app/(windows)/index.tsx
@@ -2,14 +2,19 @@ import {Animated, Image, Text, View} from "react-native";
 import {images} from "@/assets/constants/images";
 import ScrollView = Animated.ScrollView;
 import {icons} from "@/assets/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
   return (
     <View className="flex-1 bg-black">
         <Image source={images.bg} className="absolute w-full h-full z-0"/>  {/* background image*/}
 
-        <ScrollView className="flex-1 px-5">
+        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom:10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>  {/*home page logo*/}
+
+            <View className="flex-1 mt-5">
+                <SearchBar/>
+            </View>
         </ScrollView>
     </View>
   );
