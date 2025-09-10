8c251f8d6db8514d14fc4bb355f1efbeba046caf
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-10 14:17:26 +0530

    redirect to search

--- app/(windows)/index.tsx
+++ app/(windows)/index.tsx
@@ -3,8 +3,10 @@ import {images} from "@/assets/constants/images";
 import ScrollView = Animated.ScrollView;
 import {icons} from "@/assets/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
 
 export default function Index() {
+    const router = useRouter();
   return (
     <View className="flex-1 bg-black">
         <Image source={images.bg} className="absolute w-full h-full z-0"/>  {/* background image*/}
@@ -13,7 +15,10 @@ export default function Index() {
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>  {/*home page logo*/}
 
             <View className="flex-1 mt-5">
-                <SearchBar/>
+                <SearchBar
+                    onPress={() => router.push("/search")}  /*once press move to the search*/
+                    placeholder="Jurassic World"
+                />
             </View>
         </ScrollView>
     </View>
