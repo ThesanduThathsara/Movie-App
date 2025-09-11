278ab749c20deb7c377f44ca786cb926451e0b1c
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-11 22:13:47 +0530

    error correction

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -6,7 +6,7 @@ const MovieCard = ({id,poster_path,title,vote_average,release_date}:Movie) => {
 
     console.log(poster_path);
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
         <TouchableOpacity className="w-[30%]">
             <Image
                 source={{
