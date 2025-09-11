a81afe34e69ee14661c6621bb32a3e5f8f4aae16
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-11 18:47:09 +0530

    update

--- app/(windows)/index.tsx
+++ app/(windows)/index.tsx
@@ -1,12 +1,22 @@
-import {Animated, Image, Text, View} from "react-native";
+import {ActivityIndicator, Animated, Image, Text, View} from "react-native";
 import {images} from "@/assets/constants/images";
 import ScrollView = Animated.ScrollView;
 import {icons} from "@/assets/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
 
 export default function Index() {
+
     const router = useRouter();
+
+    const {
+        data:movies,
+        loading:moviesLoading,
+        error:moviesError}=useFetch(()=>fetchMovies({query:''}
+    ))
+
   return (
     <View className="flex-1 bg-black">
         <Image source={images.bg} className="absolute w-full h-full z-0"/>  {/* background image*/}
@@ -14,12 +24,25 @@ export default function Index() {
         <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom:10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>  {/*home page logo*/}
 
-            <View className="flex-1 mt-5">
-                <SearchBar
-                    onPress={() => router.push("/search")}  /*once press move to the search*/
-                    placeholder="Jurassic World"
+            {moviesLoading ?(
+                <ActivityIndicator
+                    size="large"
+                    color="#0000ff"
+                    className="mt-10 self-center"
                 />
-            </View>
+            ):moviesError?(
+                <Text>Error: {moviesError?.message}</Text>
+            ):(
+                <View className="flex-1 mt-5">
+                    <SearchBar
+                        onPress={() => router.push("/search")}  /*once press move to the search*/
+                        placeholder="Jurassic World"
+                    />
+                    <>
+                        <Text className="text-lg text-white font-bold mt-5 mb-3 ">Latest Movies</Text>
+                    </>
+                </View>
+            )}
         </ScrollView>
     </View>
   );
