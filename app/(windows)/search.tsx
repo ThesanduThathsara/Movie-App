f39e08da3217f5a789cc00f091e8f9e87a359d87
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-12 20:32:33 +0530

    create the layout of search tab

--- app/(windows)/search.tsx
+++ app/(windows)/search.tsx
@@ -1,14 +1,69 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {Image, View, Text, FlatList, ActivityIndicator} from 'react-native';
+import {images} from "@/assets/constants/images";
+import MovieCard from "@/components/MovieCard";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import {icons} from "@/assets/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
 
 const search = () => {
+
+    const router = useRouter();
+
+    const {
+        data:movies,
+        loading,
+        error}=useFetch(()=>fetchMovies({query:''}
+    ))
   return (
-    <View>
-      <Text>search</Text>
+    <View className="flex-1 bg-black">
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode={"cover"}/>
+
+        <FlatList
+            data={movies} renderItem={({item})=><MovieCard{...item}/>}
+            keyExtractor={(item)=>item.id.toString()}
+            className="px-5"
+            numColumns={3}
+            columnWrapperStyle={{
+                justifyContent:'center',
+                gap:16,
+                marginVertical:16
+            }}
+            contentContainerStyle={{paddingBottom:100}}
+            ListHeaderComponent={   /*render in the top of all the items*/
+                <>
+                    <View className="w-full flex-row justify-center mt-20 items-center">
+                        <Image source={icons.logo} className="w-12 h-10"/>
+
+                    </View>
+
+                    <View className="my-5">
+                        <SearchBar placeholder="Search movies..."/>
+                    </View>
+
+                    {loading && (
+                        <ActivityIndicator size="large" color="#0000ff" className="my-3"/>
+                    )}
+
+                    {error &&(
+                        <Text className="text-red-500 px-5 my-3">
+                            Error: {error.message}
+                        </Text>
+                    )}
+
+                    {!loading && !error && 'SEARCH TERM'.trim() && movies?.length > 0 &&(
+                        <Text className="text-xl text-white font-bold">
+                            Search result for{' '}
+                            <Text className="text-violet-400">Search Term</Text>
+                        </Text>
+                    )}
+                </>
+            }
+        />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default search;
